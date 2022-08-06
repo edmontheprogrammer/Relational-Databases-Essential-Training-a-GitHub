c926ec713b57be492157398488883660d5104052
edmontheprogrammer <41594880+edmontheprogrammer@users.noreply.github.com>
2022-08-06 22:31:36 +0800

    @edmontheprogrammer
    Relational Databases Essential Training Course Commit

---
+++ QuickDBD-export_SQL_Server_export.sql
@@ -0,0 +1,40 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [Products] (
+    [ProductName] varchar(100)  NOT NULL ,
+    [PartNumber] int  NOT NULL ,
+    [Size] varchar(20)  NOT NULL ,
+    [Color] varchar(20)  NOT NULL ,
+    [Price] money  NOT NULL ,
+    [SupplierID] int  NOT NULL ,
+    [QuantityInStock] int  NOT NULL ,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED (
+        [PartNumber] ASC
+    )
+)
+
+CREATE TABLE [Suppliers] (
+    [SupplierID] int  NOT NULL ,
+    [SupplierName] varchar(100)  NOT NULL ,
+    [PhoneNumber] char(10)  NOT NULL ,
+    [StreetAddress] varchar(100)  NOT NULL ,
+    [City] varchar(50)  NOT NULL ,
+    [State] char(2)  NOT NULL ,
+    [Zip] char(9)  NOT NULL ,
+    CONSTRAINT [PK_Suppliers] PRIMARY KEY CLUSTERED (
+        [SupplierID] ASC
+    )
+)
+
+ALTER TABLE [Products] WITH CHECK ADD CONSTRAINT [FK_Products_SupplierID] FOREIGN KEY([SupplierID])
+REFERENCES [Suppliers] ([SupplierID])
+
+ALTER TABLE [Products] CHECK CONSTRAINT [FK_Products_SupplierID]
+
+COMMIT TRANSACTION QUICKDBD
---
+++ QuickDBD-export_portgreSQL_Server_export.sql
@@ -0,0 +1,33 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Products" (
+    "ProductName" varchar(100)   NOT NULL,
+    "PartNumber" int   NOT NULL,
+    "Size" varchar(20)   NOT NULL,
+    "Color" varchar(20)   NOT NULL,
+    "Price" money   NOT NULL,
+    "SupplierID" int   NOT NULL,
+    "QuantityInStock" int   NOT NULL,
+    CONSTRAINT "pk_Products" PRIMARY KEY (
+        "PartNumber"
+     )
+);
+
+CREATE TABLE "Suppliers" (
+    "SupplierID" int   NOT NULL,
+    "SupplierName" varchar(100)   NOT NULL,
+    "PhoneNumber" char(10)   NOT NULL,
+    "StreetAddress" varchar(100)   NOT NULL,
+    "City" varchar(50)   NOT NULL,
+    "State" char(2)   NOT NULL,
+    "Zip" char(9)   NOT NULL,
+    CONSTRAINT "pk_Suppliers" PRIMARY KEY (
+        "SupplierID"
+     )
+);
+
+ALTER TABLE "Products" ADD CONSTRAINT "fk_Products_SupplierID" FOREIGN KEY("SupplierID")
+REFERENCES "Suppliers" ("SupplierID");
+
